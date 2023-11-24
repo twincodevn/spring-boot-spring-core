b7887cfb1268a010592f98a53f5d4ce37166b3ca
Bravee06 <bravee300@gmail.com>
2023-11-25 05:27:09 +0700

    04-qualifier

---
+++ 04-qualifier/src/main/java/com/twincode/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,11 @@
+package com.twincode.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Tennis daily workout";
+    }
+}
