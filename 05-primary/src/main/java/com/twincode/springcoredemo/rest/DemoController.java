0d01b9389cd2dd34a9797cae7a126f9f74842671
Bravee06 <bravee300@gmail.com>
2023-11-25 05:35:39 +0700

    05-primary

--- 05-primary/src/main/java/com/twincode/springcoredemo/rest/DemoController.java
+++ 05-primary/src/main/java/com/twincode/springcoredemo/rest/DemoController.java
@@ -12,7 +12,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach theCoach){
+    public DemoController( Coach theCoach){
         myCoach = theCoach;
     }
 
