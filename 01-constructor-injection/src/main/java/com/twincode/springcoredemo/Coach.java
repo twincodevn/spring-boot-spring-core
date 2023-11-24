5d68f880cd2b2b6cabcba2d785cd49c7592e99e3
Bravee06 <bravee300@gmail.com>
2023-11-25 05:08:10 +0700

    02-component-scanning

---
+++ 01-constructor-injection/src/main/java/com/twincode/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.twincode.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-component-scanning/src/main/java/com/twincode/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,18 @@
+package com.twincode.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(
+//		scanBasePackages =
+//				{"com.twincode.springcoredemo","com.twincode.util"}
+//				)
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
