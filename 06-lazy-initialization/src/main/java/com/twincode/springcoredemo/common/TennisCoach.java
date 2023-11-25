aefd8cb5eef547f91e888d78d536891917c9e062
Bravee06 <bravee300@gmail.com>
2023-11-25 14:32:00 +0700

    06-lazy-initialization-p2

--- 06-lazy-initialization/src/main/java/com/twincode/springcoredemo/common/TennisCoach.java
+++ 06-lazy-initialization/src/main/java/com/twincode/springcoredemo/common/TennisCoach.java
@@ -1,8 +1,10 @@
 package com.twincode.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class TennisCoach implements Coach {
 
     public TennisCoach(){
